dde61a34995e90caff77008954aa9ead2950298f
mok666 <ieei.47.abc@gmail.com>
2020-10-07 09:44:02 +0900

    v0.1

--- app/app.mjs
+++ app/app.mjs
@@ -1,24 +1,11 @@
 //install library
-import http from "http";
-import path from "path";
-import express from "express";
 import io from "socket.io";
 import json from "./modules/get-iss-pojition-api.mjs";
-const dirname = path.dirname(new URL(import.meta.url).pathname);
-let app = express();
-let wss = new io();
 let port = process.env.Port || 7000;
-
-app.use(express.static(`${dirname}/`));
-
-let server = http.createServer(app);
-server.listen(port);
-
-console.log("http server listening on %d", port);
-
+let socket = io(port);
 
 //wait websocket
-wss.on('connection', (socket) => {
+socket.on('connection', (socket) => {
   console.log("user connect!");
   let pushflag = true;
   while(pushflag == true){
@@ -31,4 +18,4 @@ wss.on('connection', (socket) => {
   });
 });
 
-console.log('Start socket server : http://127.0.0.1:8023');
+console.log(`Start socket server : http://localhost:${port}`);
